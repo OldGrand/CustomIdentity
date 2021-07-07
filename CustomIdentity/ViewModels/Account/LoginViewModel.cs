0c10f4b72821ca695603a42b8c4e3d7636d81343
Sergey Sipov <37745809+OldGrand@users.noreply.github.com>
2021-07-07 15:40:44 +0300

    Remove view models

--- CustomIdentity/ViewModels/Account/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomIdentity.ViewModels.Account
-{
-    public class LoginViewModel
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public bool RememberMe { get; set; }
-    }
-}
--- CustomIdentity/ViewModels/Account/RegisterViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomIdentity.ViewModels.Account
-{
-    public class RegisterViewModel
-    {
-        public string Email { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
